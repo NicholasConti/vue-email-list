d00196dd194699aaa0758131d2da1ee695ec15d7
NicholasConti <conti94@live.it>
2023-03-21 16:02:54 +0100

    creazione lista mail random

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            message: 'Ciaooooooooooooo'
+            emails: []
+        }
+    },
+    beforeMount() {
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.emails.push(response.data.response);
+            });
         }
     }
 }).mount('#app');
